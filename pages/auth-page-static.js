429aa50dac8784de4dc8dc39e863576c8673c0c0
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-20 11:19:25 -0300

    Validating session in static page

--- pages/auth-page-static.js
+++ pages/auth-page-static.js
@@ -1,4 +1,56 @@
-export default function AuthPageStatic(props){
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { authService } from "../src/services/auth/authService";
+
+function useSession(){
+  const [session, setSession] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    authService.getSession()
+      .then((session) => {
+        setSession(session);
+      })
+      .catch(err => {
+        setError(err)
+      })
+      .finally(() => {
+        setloading(false);
+      })
+  }, [])
+
+  return {
+    data: {
+      session
+    },
+    error,
+    loading
+  }
+}
+
+function withSessionHOC(Component){
+  return function Wrapper(props){
+    const router = useRouter();
+    const session = useSession();
+
+    if (!session.loading && session.error) {
+      router.push("/?error=401");
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session
+    }
+
+      return (
+        <Component {...modifiedProps} />
+      )
+  }
+}
+
+function AuthPageStatic(props){
+
   return (
     <div>
       <h1>
@@ -10,3 +62,5 @@ export default function AuthPageStatic(props){
     </div>
   )
 }
+
+export default withSessionHOC(AuthPageStatic)
--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -15,7 +15,7 @@ export const authService = {
       tokenService.saveToken(body.data.access_token);
     })
   },
-  async getSession(ctx){
+  async getSession(ctx = null){
     const token = tokenService.getToken(ctx);
     return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/session`, {
       method: 'GET',
