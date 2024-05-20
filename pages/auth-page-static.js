9504c97065e623eee80e80d4881f6b64ad257f92
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-20 11:22:38 -0300

    Using 'useSession' and 'withSessionsHOC' in fyle session.js

--- pages/auth-page-static.js
+++ pages/auth-page-static.js
@@ -1,56 +1,6 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { authService } from "../src/services/auth/authService";
-
-function useSession(){
-  const [session, setSession] = useState(null);
-  const [loading, setloading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    authService.getSession()
-      .then((session) => {
-        setSession(session);
-      })
-      .catch(err => {
-        setError(err)
-      })
-      .finally(() => {
-        setloading(false);
-      })
-  }, [])
-
-  return {
-    data: {
-      session
-    },
-    error,
-    loading
-  }
-}
-
-function withSessionHOC(Component){
-  return function Wrapper(props){
-    const router = useRouter();
-    const session = useSession();
-
-    if (!session.loading && session.error) {
-      router.push("/?error=401");
-    }
-
-    const modifiedProps = {
-      ...props,
-      session: session.data.session
-    }
-
-      return (
-        <Component {...modifiedProps} />
-      )
-  }
-}
+import { withSessionHOC } from "../src/services/auth/session"
 
 function AuthPageStatic(props){
-
   return (
     <div>
       <h1>
--- src/services/auth/session.js
+++ src/services/auth/session.js
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import { authService } from "./authService";
+import { useEffect, useState } from "react";
 
 export function withSession(func){
   return async (ctx) => {
@@ -21,4 +23,51 @@ export function withSession(func){
     }
   }
 }
+}
+
+export function useSession(){
+  const [session, setSession] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    authService.getSession()
+      .then((session) => {
+        setSession(session);
+      })
+      .catch(err => {
+        setError(err)
+      })
+      .finally(() => {
+        setloading(false);
+      })
+  }, [])
+
+  return {
+    data: {
+      session
+    },
+    error,
+    loading
+  }
+}
+
+export function withSessionHOC(Component){
+  return function Wrapper(props){
+    const router = useRouter();
+    const session = useSession();
+
+    if (!session.loading && session.error) {
+      router.push("/?error=401");
+    }
+
+    const modifiedProps = {
+      ...props,
+      session: session.data.session
+    }
+
+      return (
+        <Component {...modifiedProps} />
+      )
+  }
 }
